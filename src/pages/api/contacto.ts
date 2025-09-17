a7bca7b00901719b3a57e0a2e439c6e692cd2bbf
Santilook <santinahuel200@gmail.com>
2025-09-17 20:43:03 -0300

    Se añadio seccion de contacto con implemantacion de api para enviar gmails

---
+++ src/pages/api/contacto.ts
@@ -0,0 +1,49 @@
+import 'dotenv/config';
+
+export const prerender = false;
+
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "Cargada" : "Falta");
+
+
+import type { APIRoute } from "astro";
+import nodemailer from "nodemailer";
+
+export const POST: APIRoute = async ({ request }) => {
+  try {
+    const { nombre, email, mensaje } = await request.json();
+
+    if (!nombre?.trim() || !email?.trim() || !mensaje?.trim()) {
+      return new Response(
+        JSON.stringify({ error: "Todos los campos son obligatorios" }),
+        { status: 400 }
+      );
+    }
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: `"${nombre}" <${email}>`,
+      to: process.env.EMAIL_USER,
+      subject: "Nuevo mensaje desde formulario de contacto",
+      text: mensaje,
+    });
+
+    return new Response(
+      JSON.stringify({ message: "Mensaje enviado correctamente" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ error: "Error al enviar el mensaje" }),
+      { status: 500 }
+    );
+  }
+};
